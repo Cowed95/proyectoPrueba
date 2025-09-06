1573855e0a7edcbe42a8e8104e54dbf1a166982a
Camilo <marincamilo505@gmail.com>
2025-09-06 11:20:51 -0500

    add README.md

---
+++ src/main/java/com/trabajoproyecto/demo/entity/User.java
@@ -0,0 +1,22 @@
+package com.trabajoproyecto.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor @Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
